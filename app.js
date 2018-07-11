65844bd0a45b90f782ea023a0a59eb9c862a5d44
Mikaiyl <mikaiyl.davis@icloud.com>
2018-07-11 08:16:48 -0400

    fin

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
+const PORT = 3000
+
 const express = require("express")
 const multer = require("multer")
+const fs = require('fs')
 
-const publicPath = "./public/"
-const port = 3000
+const uploadPath = "./public/uploads"
+const publicPath = "./public"
 
 const app = express()
+const upload = multer({ dest: uploadPath })
+
 app.use(express.static(publicPath))
-const upload = multer({ dest: publicPath })
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 const uploadedFiles = []
 
-app.listen(port)
+app.get( '/kg', ( req, res ) => {
 
-app.post('/upload', upload.single('pikature'), function (request, response, next) {
-    // request.file is the `myFile` file
+    fs.readdir(uploadPath, function(err, items) {
+        console.log(items);
+        res.render(
+            'kg',
+            {
+                title: 'KG',
+                items: items,
+            }
+        );
+    });
+
+} )
+
+app.post( '/upload', upload.single('pikature'), function (request, response, next) {
+
     // request.body will hold the text fields, if there were any
     console.log("Uploaded: " + request.file.filename)
     uploadedFiles.push(request.file.filename)
-    response.end("Uploaded file!")
+    response.render( 'upload' )
 
+    // if callback call on res
     if ( next ) {
         next( response )
     }
-})
 
+} )
+
+app.listen( PORT )
 
